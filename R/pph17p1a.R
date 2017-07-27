0c4e7a7683e19c531bf77c85c62a4c43537e4ae4
Yudha H Tejaningrat <yudha.tejaningrat@pajak.go.id>
2017-07-27 17:17:11 +0700

    Functions: Personal Income Tax based on table [fix]

--- R/pph17p1a.R
+++ R/pph17p1a.R
@@ -1,21 +1,12 @@
-pph17p1a <- function(pkp) {
-  if (pkp < 0) {
-    pph <- 0
-    return(pph)
-  } else if (pkp > 0 && pkp <= 5e+7) {
-    pph <- pkp * 5 / 100
-    return(pph)
-  } else if (pkp > 5e+7 && pkp <= 2.5e+8) {
-    pph <- 5e+7 * 5 / 100
-    pph <- pph + ((pkp - 5e+7) * 15 / 100)
-    return(pph)
-  } else if (pkp > 2.5e+8 && pkp <= 5e+8) {
-    pph <- (5e+7 * 5 / 100) + (2e+8 * 15 / 100)
-    pph <- pph + ((pkp - 2.5e+8) * 25 / 100)
-    return(pph)
-  } else if (pkp > 5e+8) {
-    pph <- (5e+7 * 5 / 100) + (2e+8 * 15 / 100) + (2.5e+8 * 25 / 100)
-    pph <- pph + ((pkp - 5e+8) * 30 / 100)
-    return(pph)
+pph17p1a <- function(pkp, braket=tbl_pph17p1a) {
+  PPh <- 0
+  for(i in 1:nrow(braket)) {
+    if(pkp <= braket[i, c("Max")] && pkp > braket[i, c("Min")]) {
+      Min = braket[i, c("Min")]
+      Scale = pkp - Min
+      Trf = braket[i, c("Tarif")]
+      PPh = braket[i, c("Vmin")] + (Scale * Trf)
+    }
   }
+  return(PPh)
 }
